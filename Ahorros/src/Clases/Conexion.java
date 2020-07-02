04950d1bef6d47d0ac195c07e9b8153639eaabf9
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2020-07-01 21:18:58 -0600

    :v:

--- Ahorros/src/Clases/Conexion.java
+++ Ahorros/src/Clases/Conexion.java
@@ -1,8 +1,42 @@
 package Clases;
 
 import com.mysql.jdbc.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 
 public class Conexion {
     
     private static Connection conn;
+    private static final String driver = "com.mysql.jdbc.Driver";
+    private static final String User = "root";
+    private static final String Password = "1234";
+    private static final String url = "jdbc:mysql//localhost:3306/ahorrosbd";
+    
+    public Conexion(){
+        conn = null;
+        try{
+            Class.forName(driver);
+            conn = (Connection) DriverManager.getConnection(url, User, Password);
+            
+            if(conn != null){
+                System.out.println("¡Conectado corrctamente!");
+            }
+        }
+        catch(ClassNotFoundException | SQLException ex){
+            System.out.println("¡Error al Conectar a la Base de Datos!");
+        }
+    }
+    
+    public Connection getConnection(){
+        return conn;
+    }
+    
+    public void Desconectar(){
+        conn = null;
+        
+        if(conn == null){
+            System.out.println("¡Deconectado con éxito!");
+        }
+    }
 }
