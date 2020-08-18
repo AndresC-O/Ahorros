ac6a5153ec49db10d9ec238face6b06523a65257
AndresC-O <62961669+AndresC-O@users.noreply.github.com>
2020-08-18 11:38:42 -0600

    Adición de un nuevo Hosting
    FREEHOSTING

--- Ahorros/src/Clases/Conexion.java
+++ Ahorros/src/Clases/Conexion.java
@@ -8,15 +8,20 @@ import java.sql.SQLException;
 public class Conexion {
     
     //PARA ACCEDER A LOCALHOST
-//    private static Connection conn;
-//    private static final String User = "root";
-//    private static final String Password = "root";
-//    private static final String url = "jdbc:mysql://localhost:3306/ahorrosbd";
-    //PARA ACCEDER A WEB SERVER
     private static Connection conn;
-    private static final String User = "sql9358524";
-    private static final String Password = "aEpPtPGZJi";
-    private static final String url = "jdbc:mysql://sql9.freemysqlhosting.net/sql9358524";
+    private static final String User = "root";
+    private static final String Password = "root";
+    private static final String url = "jdbc:mysql://localhost:3306/ahorrosbd";
+    //PARA ACCEDER A WEB SERVER
+//    private static Connection conn;
+//    private static final String User = "sql9358524";
+//    private static final String Password = "aEpPtPGZJi";
+//    private static final String url = "jdbc:mysql://sql9.freemysqlhosting.net/sql9358524";
+    //PARA ACCEDER A FREEHOSTING
+//    private static Connection conn;
+//    private static final String User = "epiz_26532749";
+//    private static final String Password = "GwvlIfBJQa";
+//    private static final String url = "jdbc:mysql://sql313.epizy.com/epiz_26532749_ahorrosbd";
     //PARA ACCEDER A IP SERVER
 //    private static Connection conn;
 //    private static final String User = "root";
